95a06d3a834ff05b5b0e66adfbcddc2e93c004af
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2026-02-16 20:30:23 +0100

    added water body class

---
+++ src/main/java/com/mohamed/lock/model/WaterBody.java
@@ -0,0 +1,14 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+package com.mohamed.lock.model;
+
+public class WaterBody {
+   private int level;
+
+   public WaterBody(int var1) {
+      this.level = var1;
+   }
+
+   public int getLevel() {
+      return this.level;
+   }
+}
