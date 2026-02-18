14fa057271f9d4b8629cbc83d4292984f74af532
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2026-02-18 19:34:41 +0100

    add valve class

---
+++ src/main/java/com/mohamed/lock/model/Valve.java
@@ -0,0 +1,19 @@
+package com.mohamed.lock.model;
+
+public class Valve {
+    private double opening;
+
+    public void setOpening(double opening){
+        if (opening < 0.0 || opening > 1.0) throw new IllegalArgumentException("Opening must be in [0,1]");
+
+        this.opening = opening;
+    }
+
+    public double getOpening(){
+        return opening;
+    }
+
+    public boolean isOpen(){
+        return opening > 0.0;
+    }
+}
