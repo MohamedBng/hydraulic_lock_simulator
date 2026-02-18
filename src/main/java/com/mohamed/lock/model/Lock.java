644881eb378e77e3a4706bf0ef7f6e3a3fd4c9b7
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2026-02-18 20:37:13 +0100

    Lock class wip

---
+++ src/main/java/com/mohamed/lock/model/Lock.java
@@ -0,0 +1,21 @@
+package com.mohamed.lock.model;
+
+public class Lock {
+    private Door upstreamDoor;
+    private Door downstreamDoor;
+    private WaterBody saasWater;
+    private WaterBody upstreamWater;
+    private WaterBody downstreamWater;
+
+    public void openUpstreamDoor(){
+    }
+
+    public void closeUpstreamDoor(){
+    }
+
+    public void openDownstreamDoor(){
+    }
+
+    public void closeDownstreamDoor(){
+    }
+}
