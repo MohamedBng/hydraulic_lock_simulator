b87fcb985abaa71c931682bf17596ff4f78bd76d
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2026-02-18 20:36:19 +0100

    add constructor for valve

--- src/main/java/com/mohamed/lock/model/Valve.java
+++ src/main/java/com/mohamed/lock/model/Valve.java
@@ -3,6 +3,10 @@ package com.mohamed.lock.model;
 public class Valve {
     private double opening;
 
+    public Valve(){
+        setOpening(0.0);
+    }
+
     public void setOpening(double opening){
         if (opening < 0.0 || opening > 1.0) throw new IllegalArgumentException("Opening must be in [0,1]");
 
